cc46edac088dc9f5167e2d2cebc848d137ca5802
Gabriel Figueiredo <gabrielcarolino962@gmail.com>
2022-05-02 23:13:19 -0300

    IMPROV: Populating data from front to get students

--- src/pages/Students/index.js
+++ src/pages/Students/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './styles.css';
 import logoRegister from '../../assets/cadastro1.png'
+import api from '../../services/api';
 import {Link} from 'react-router-dom';
 import {FiXCircle, FiEdit, FiUserX} from 'react-icons/fi'
+import { useHistory } from 'react-router-dom';
 
 export default function Students(){
+
+    const [name, setName] = useState('');
+    const [students, setStudents] = useState([]);
+
+    const email = localStorage.getItem('email');
+    const token = localStorage.getItem('token');
+
+    const authorization = {
+        headers : {
+            Authorization : `Bearer ${token}`
+        }
+    }
+
+    const history = useHistory();
+
+    useEffect( ()=> {
+        api.get('api/student', authorization).then(
+            response => {setStudents(response.data)
+            }, token)
+    })
+
+    async function logout() {
+        try{
+
+            localStorage.clear();
+            localStorage.setItem('token', '');
+            authorization.headers = '';
+            history.push('/');
+            
+        }catch(error){
+            alert('logout error' + error)
+        }
+    }
+
     return(
         <div className="student-container">
             <header>
                 <img src = {logoRegister} alt="Register" />
-                <span>Welcome, <strong>Student</strong>!</span>
+                <span>Welcome, <strong>{email}</strong>!</span>
                 <Link className="button" to="student/new/0">New Student</Link>
-                <button type="button">
+                <button onClick={logout} type="button">
                     <FiXCircle size={35} color="#17202A"/>
                 </button>
             </header>
@@ -23,29 +59,21 @@ export default function Students(){
             </form>
             <h1>Students Relationship</h1>
             <ul>
-                <li>
-                    <b>Name: </b>Pauladinha<br/><br/>
-                    <b>Email: </b>aisojdioas@gmail.com<br/><br/>
-                    <b>Age: </b>22<br/><br/>
-                    <button type="button">
-                        <FiUserX size="25" color="#17202A"/>
-                    </button>
-                    <button type="button">
-                        <FiEdit size="25" color="#17202A"/>
-                    </button>
-                </li>
+                {students.map(student => (
+                    <li key={student.id}>
+                        <b>Name:</b> {student.name} <br/><br/>
+                        <b>Email:</b> {student.email} <br/><br/>
+                        <b>Age:</b> {student.age} <br/><br/>
 
-                <li>
-                    <b>Name: </b>Pauladinha<br/><br/>
-                    <b>Email: </b>aisojdioas@gmail.com<br/><br/>
-                    <b>Age: </b>22<br/><br/>
-                    <button type="button">
-                        <FiUserX size="25" color="#17202A"/>
-                    </button>
-                    <button type="button">
-                        <FiEdit size="25" color="#17202A"/>
-                    </button>
-                </li>
+                        <button type="button">
+                            <FiUserX size="25" color="#17202A"/>
+                        </button>
+
+                        <button type="button">
+                            <FiEdit size="25" color="#17202A"/>
+                        </button>
+                    </li>
+                ))}      
             </ul>
         </div>
     )
